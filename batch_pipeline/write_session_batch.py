66d6252863ae4a2a78054f7cded49a549dc006cd
Prakash Dontaraju <prakashdontaraju@gmail.com>
2020-11-11 19:34:17 -0600

    starting connection to spanner
    starting connection to spanner

--- batch_pipeline/write_session_batch.py
+++ batch_pipeline/write_session_batch.py
@@ -1,14 +1,13 @@
 import json
 import logging
 import argparse
+#  import shortuuid
 import numpy as np
 import pandas as pd
-import sqlalchemy as db
+from google.cloud import spanner
 from pyspark.sql import Row, SQLContext
+from google.cloud.spanner_v1 import param_types
 from pyspark import SparkConf, SparkContext
-from pyspark.sql.functions import col, hour, lit, split as Split
-from pyspark.sql.types import FloatType, StringType, TimestampType
-# from sqlalchemy import MetaData, Table, Column, Float, Integer, String, DateTime
 
 
 
@@ -18,26 +17,14 @@ product_attributes = ['category', 'sub_category', 'product','product_details']
 
 def get_product_information(row, product_attributes):
 
+    category_code = row.category_code
 
-    # fill_category_details = [np.nan, np.nan, np.nan, np.nan]
-    
+    details = category_code.split('.')
+
     row = row.asDict()
 
-    category_details = row['category_code'].split('.')
-
-    
-    row['category_code'] = dict(zip(product_attributes, category_details))
-
-    row['category'] = row['category_code'].get('category', np.nan)
-
-    row['sub_category'] = row['category_code'].get('sub_category', np.nan)
-
-    # row['product'] = row['category_code'].get('product', np.nan)
-
-    # row['product_details'] = row['category_code'].get('product_details', np.nan)
-    
-    row['category_code'] = json.dumps(row['category_code'])
-
+    row['category_code'] = dict(zip(product_attributes, details))
+    # row['category_code'] = json.dumps(row['category_code'])
 
     return Row(**row)
 
@@ -47,18 +34,11 @@ def transform_data(sqlContext, user_sessions_chunk_df, product_attributes):
 
     # column-level transformations quicker with Spark Dataframes vs RDDs
     user_sessions_spDF = sqlContext.createDataFrame(user_sessions_chunk_df.astype(str))
-    # user_sessions_spDF.fillna(np.nan)
+    user_sessions_spDF.fillna(np.nan)
     user_sessions_spDF = user_sessions_spDF.withColumn(
                             'event_time', user_sessions_spDF['event_time'].cast(TimestampType()))
     user_sessions_spDF = user_sessions_spDF.withColumn(
                             'price', user_sessions_spDF['price'].cast(FloatType()))
-    user_sessions_spDF = user_sessions_spDF.withColumn(
-                            'hour', hour(col('event_time')))
-    user_sessions_spDF = user_sessions_spDF.withColumn(
-                            'category', lit(None).cast(StringType()))
-    user_sessions_spDF = user_sessions_spDF.withColumn(
-                            'sub_category', lit(None).cast(StringType()))
-    
 
     # some element-wise or row-wise operations are best with RDDs
     user_sessions_rdd = user_sessions_spDF.rdd.map(list)
@@ -68,116 +48,89 @@ def transform_data(sqlContext, user_sessions_chunk_df, product_attributes):
 
     user_sessions_spDF = user_sessions_rdd.toDF()
 
-    # print(user_sessions_rdd.take(15))
-
-    # print(user_sessions_spDF.take(15))
-
     return user_sessions_spDF
 
 
+def create_database(instance_id, database_id):
+    """Creates a database and tables for sample data."""
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(instance_id)
 
-def cloudsql_connection(cloudsql_user, cloudsql_password, project,
-                            cloudsql_database, cloudsql_ip, cloudsql_instance):
+    database = instance.database(
+        database_id,
+        ddl_statements=[
+            """CREATE TABLE batch_data ( 
+                        record_id INT64 NOT NULL, 
+                        event_time TIMESTAMP NOT NULL, 
+                        event_type STRING(20) NOT NULL, 
+                        product_id STRING(30) NOT NULL, 
+                        category_id STRING(30) NOT NULL, 
+                        category_code STRING(200) NOT NULL, 
+                        brand STRING(30) NOT NULL, 
+                        price FLOAT64 NOT NULL, 
+                        user_id STRING(200) NOT NULL, 
+                        user_session STRING(200) NOT NULL, 
+                        ) PRIMARY KEY (record_id), 
+                    """
+        ],
+    )
+
+    operation = database.create()
+
+    print("Waiting for operation to complete...")
+    operation.result(120)
+
+    print("Created database {} on instance {}".format(database_id, instance_id))
 
 
-    config = {
-    
-        'user': '{0}'.format(cloudsql_user),
-        'password': '{0}'.format(cloudsql_password),
-        'project': '{0}'.format(project),
-        'database': '{0}'.format(cloudsql_database),
-        'ip_address': '{0}'.format(cloudsql_ip),
-        'instance': '{0}'.format(cloudsql_instance)
-        # 'host': '{0}'.format(cloudsql_host)
-    }
-
-
-    cloudsql_user = config.get('user')
-    cloudsql_password = config.get('password')
-    project = config.get('project')
-    cloudsql_database = config.get('database')
-    cloudsql_ip = config.get('ip_address')
-    cloudsql_instance = config.get('instance')
-
-    connection_config = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(cloudsql_user,
-                            cloudsql_password, cloudsql_ip, cloudsql_database)
-    
-    # connection_config = 'mysql+pymysql://{0}:{1}@/{2}?unix_socket=/cloudsql/{3}'.format(
-    #                           cloudsql_user, cloudsql_password, cloudsql_database,
-    #                               cloudsql_instance)
-
-    # connection_config = 'mysql+pymysql://{0}@{1}/{2}'.format(cloudsql_user,
-    #                                                 cloudsql_database, cloudsql_ip)
-    # print(connection_config)
-    
-    # connect to database
-    cloudsql_engine = db.create_engine(connection_config)
-    cloudsqlConnection = cloudsql_engine.connect()
-
-    return cloudsqlConnection
+#  shortuuid.ShortUUID().random(length=5), spanner.COMMIT_TIMESTAMP
 
 
 
-def write_to_cloudsql(cloudsqlConnection, cloudsql_table, user_sessions_spDF):
+def write_to_spanner(database, transaction, row):
 
-    user_sessions_df = user_sessions_spDF.toPandas()
-    # print(user_sessions_df.head(15))
-    user_sessions_df.to_sql(con=cloudsqlConnection, name=cloudsql_table,
-                                if_exists='append', index=False)
+    def insert_session(transaction):
+        session = transaction.execute_update(
+            "INSERT batch_data"
+                "(record_id, event_time, event_type, product_id, category_id"
+                "category_code, brand, price, user_id, user_session) "
+                "VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}".format(
+                    spanner.COMMIT_TIMESTAMP,row['event_time'], row['event_type'],
+                    row['product_id'], row['category_id'], row['category_code'], row['brand'],
+                    row['price'], row['user_id'], row['user_session'])
+        )
+
+    database.run_in_transaction(insert_session)
 
 
 
 def main():
 
     parser = argparse.ArgumentParser(
-        description='Perform Batch processing to send session data to Cloud SQL')
+        description='Perform Batch processing to send session data to Spanner')
 
     parser.add_argument(
         '--input',
-        help='Path to file in GCS bucket. Example: --input gs://$PROJECT/$FILE',
+        help='Path to local file. Example: --input C:/Path/To/File/File.csv',
         required=True)
 
     parser.add_argument(
-        '--cloudsql_user',
-        help='Cloud SQL Database User; Example: --cloudsql_user user_admin',
-        required=True)
-
-    parser.add_argument(
-        '--cloudsql_password',
-        help='Cloud SQL Database Password; Example: --cloudsql_password user_password',
+        '--port',
+        help='Port to listen to Spanner. Example: --port 3306',
+        type=int,
         required=True)
     
-    parser.add_argument(
-        '--project',
-        help='GCP Project ID; Example: --project ecommerce',
-        required=True)
 
-    parser.add_argument(
-        '--cloudsql_database',
-        help='Cloud SQL Database Name; Example: --cloudsql_database user_sessions',
-        required=True)
+    mysqlConnection = mysql_connection()
+    mysqlCursor = mysqlConnection.cursor()
 
-    parser.add_argument(
-        '--cloudsql_ip',
-        help='Cloud SQL Instance Public IP Address; Example: --cloudsql_ip 11.22.33.444',
-        required=True)    
-    
-    parser.add_argument(
-        '--cloudsql_instance',
-        help='Cloud SQL Instance Name; Example: --cloudsql_instance batch_data',
-        required=True)
+    table_names = ['batch_data']
+    for table_name in table_names:
+        create_table(mysqlCursor, table_name)
+        # drop_table(mysqlCursor, table_name)
 
-    parser.add_argument(
-        '--cloudsql_table',
-        help='Cloud SQL Database Table; Example: --cloudsql_table batch_table',
-        required=True)
-    
+
     args = parser.parse_args()
-
-    cloudsqlConnection = cloudsql_connection(args.cloudsql_user, args.cloudsql_password,
-                            args.project, args.cloudsql_database, args.cloudsql_ip,
-                                args.cloudsql_instance)
-
 
     logging.info('Reading Dataset')
     user_sessions_chunks_df = pd.read_csv(args.input,
@@ -188,18 +141,24 @@ def main():
     sc = SparkContext(conf = conf)
     sqlContext = SQLContext(sc)
 
+    create_database(instance_id, database_id)
+
 
     for user_sessions_chunk_df in user_sessions_chunks_df:
 
         logging.info('Transforming data from the Batch')
         # print(user_sessions_chunk_df.count())
         user_sessions_spDF = transform_data(sqlContext, user_sessions_chunk_df, product_attributes)
+        # print(user_sessions_spDF.show(n=5))
+        # print(column_names)
+        user_sessions_df = user_sessions_spDF.toPandas()
 
-        logging.info('Loading DF Data from the Batch into batch_table Cloud SQL Table')
-        write_to_cloudsql(cloudsqlConnection, args.cloudsql_table, user_sessions_spDF)
+        logging.info('Loading DF Data from the Batch into batch_data Spanner Table')
+        for index, row in user_sessions_df.iterrows():
+            write_to_spanner(database, transaction, row)
 
 
-    logging.info('Finished Loading DF Data from all Batches into batch_table Cloud SQL Table')
+    logging.info('Finished Loading DF Data from all Batches into batch_data Spanner Table')
     
 
 
