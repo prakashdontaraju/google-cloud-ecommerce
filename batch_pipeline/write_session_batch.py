dafeea8e09191404dd7a6a8b9eeff89514a384fe
Prakash Dontaraju <prakashdontaraju@gmail.com>
2020-11-12 20:36:28 -0600

    created spanner database & table
    created cloud spanner database & table for pyspark batch pipeline

--- batch_pipeline/write_session_batch.py
+++ batch_pipeline/write_session_batch.py
@@ -1,12 +1,11 @@
-import json
 import logging
 import argparse
-#  import shortuuid
-import numpy as np
+import shortuuid
+# import numpy as np
 import pandas as pd
 from google.cloud import spanner
 from pyspark.sql import Row, SQLContext
-from google.cloud.spanner_v1 import param_types
+from pyspark.sql.types import FloatType, TimestampType
 from pyspark import SparkConf, SparkContext
 
 
@@ -24,7 +23,8 @@ def get_product_information(row, product_attributes):
     row = row.asDict()
 
     row['category_code'] = dict(zip(product_attributes, details))
-    # row['category_code'] = json.dumps(row['category_code'])
+    # back to unicode string
+    row['category_code'] = str(row['category_code'])
 
     return Row(**row)
 
@@ -34,11 +34,11 @@ def transform_data(sqlContext, user_sessions_chunk_df, product_attributes):
 
     # column-level transformations quicker with Spark Dataframes vs RDDs
     user_sessions_spDF = sqlContext.createDataFrame(user_sessions_chunk_df.astype(str))
-    user_sessions_spDF.fillna(np.nan)
-    user_sessions_spDF = user_sessions_spDF.withColumn(
-                            'event_time', user_sessions_spDF['event_time'].cast(TimestampType()))
-    user_sessions_spDF = user_sessions_spDF.withColumn(
-                            'price', user_sessions_spDF['price'].cast(FloatType()))
+    # user_sessions_spDF.fillna(value="None")
+    # user_sessions_spDF = user_sessions_spDF.withColumn(
+    #                         'event_time', user_sessions_spDF['event_time'].cast(TimestampType()))
+    # user_sessions_spDF = user_sessions_spDF.withColumn(
+    #                         'price', user_sessions_spDF['price'].cast(FloatType()))
 
     # some element-wise or row-wise operations are best with RDDs
     user_sessions_rdd = user_sessions_spDF.rdd.map(list)
@@ -46,59 +46,71 @@ def transform_data(sqlContext, user_sessions_chunk_df, product_attributes):
     user_sessions_rdd = user_sessions_spDF.rdd.map(
                         lambda row: get_product_information(row, product_attributes))
 
-    user_sessions_spDF = user_sessions_rdd.toDF()
+    user_sessions_df = user_sessions_rdd.toDF().toPandas()
+    # user_sessions_df = user_sessions_spDF.toPandas()
 
-    return user_sessions_spDF
+    
+
+    return user_sessions_df
 
 
-def create_database(instance_id, database_id):
-    """Creates a database and tables for sample data."""
+def get_instance(instance_id):
+
     spanner_client = spanner.Client()
     instance = spanner_client.instance(instance_id)
+
+    return instance
+
+
+
+def create_database(instance, database_id):
+    """Creates a database and tables for sample data."""
 
     database = instance.database(
         database_id,
         ddl_statements=[
             """CREATE TABLE batch_data ( 
-                        record_id INT64 NOT NULL, 
-                        event_time TIMESTAMP NOT NULL, 
-                        event_type STRING(20) NOT NULL, 
-                        product_id STRING(30) NOT NULL, 
-                        category_id STRING(30) NOT NULL, 
+                        record_id STRING(20) NOT NULL,
+                        event_time STRING(50) NOT NULL, 
+                        event_type STRING(50) NOT NULL, 
+                        product_id STRING(50) NOT NULL, 
+                        category_id STRING(50) NOT NULL, 
                         category_code STRING(200) NOT NULL, 
                         brand STRING(30) NOT NULL, 
-                        price FLOAT64 NOT NULL, 
-                        user_id STRING(200) NOT NULL, 
-                        user_session STRING(200) NOT NULL, 
-                        ) PRIMARY KEY (record_id), 
-                    """
+                        price STRING(30) NOT NULL, 
+                        user_id STRING(50) NOT NULL, 
+                        user_session STRING(100) NOT NULL, 
+                        ) PRIMARY KEY (record_id)""",
         ],
     )
 
     operation = database.create()
 
-    print("Waiting for operation to complete...")
+    logging.info("Waiting for operation to complete...")
     operation.result(120)
 
-    print("Created database {} on instance {}".format(database_id, instance_id))
+    logging.info("Created database {} on instance {}".format(database, instance))
 
 
 #  shortuuid.ShortUUID().random(length=5), spanner.COMMIT_TIMESTAMP
 
 
 
-def write_to_spanner(database, transaction, row):
+def write_to_spanner(instance, database_id, row):
+
+    database = instance.database(database_id)
 
     def insert_session(transaction):
-        session = transaction.execute_update(
-            "INSERT batch_data"
-                "(record_id, event_time, event_type, product_id, category_id"
-                "category_code, brand, price, user_id, user_session) "
-                "VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}".format(
-                    spanner.COMMIT_TIMESTAMP,row['event_time'], row['event_type'],
-                    row['product_id'], row['category_id'], row['category_code'], row['brand'],
-                    row['price'], row['user_id'], row['user_session'])
-        )
+        insert_query = """INSERT batch_data (record_id, event_time, event_type, 
+        product_id, category_id, category_code, brand, price, user_id, user_session) 
+        VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9})""".format(
+            shortuuid.ShortUUID().random(length=5), row['event_time'], 
+            row['event_type'], row['product_id'], row['category_id'], 
+            row['category_code'], row['brand'], row['price'], 
+            row['user_id'], row['user_session'])
+        session = transaction.execute_update(insert_query)
+
+        # logging.info(session)
 
     database.run_in_transaction(insert_session)
 
@@ -115,19 +127,14 @@ def main():
         required=True)
 
     parser.add_argument(
-        '--port',
-        help='Port to listen to Spanner. Example: --port 3306',
-        type=int,
+        '--instance_id',
+        help='Cloud Spanner instance ID Example: --instance_id spanner_instance',
         required=True)
-    
 
-    mysqlConnection = mysql_connection()
-    mysqlCursor = mysqlConnection.cursor()
-
-    table_names = ['batch_data']
-    for table_name in table_names:
-        create_table(mysqlCursor, table_name)
-        # drop_table(mysqlCursor, table_name)
+    parser.add_argument(
+        '--database_id',
+        help='Cloud Spanner database ID Example: --database-id spanner_database',
+        required=True)
 
 
     args = parser.parse_args()
@@ -141,21 +148,23 @@ def main():
     sc = SparkContext(conf = conf)
     sqlContext = SQLContext(sc)
 
-    create_database(instance_id, database_id)
+    instance = get_instance(args.instance_id)
+
+
+    logging.info('Creating user_sessions Spanner Database')
+    create_database(instance, args.database_id)
 
 
     for user_sessions_chunk_df in user_sessions_chunks_df:
 
         logging.info('Transforming data from the Batch')
         # print(user_sessions_chunk_df.count())
-        user_sessions_spDF = transform_data(sqlContext, user_sessions_chunk_df, product_attributes)
-        # print(user_sessions_spDF.show(n=5))
-        # print(column_names)
-        user_sessions_df = user_sessions_spDF.toPandas()
+        user_sessions_df = transform_data(sqlContext, user_sessions_chunk_df, product_attributes)
 
         logging.info('Loading DF Data from the Batch into batch_data Spanner Table')
         for index, row in user_sessions_df.iterrows():
-            write_to_spanner(database, transaction, row)
+            write_to_spanner(instance, args.database_id, row)
+            break
 
 
     logging.info('Finished Loading DF Data from all Batches into batch_data Spanner Table')
